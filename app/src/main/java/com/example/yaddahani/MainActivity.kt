129b33a9d19ec65d5845e29f3f884613741f70b6
rehmanjameel <abdulrehmancs17@gmail.com>
2022-02-07 19:26:08 +0500

    SplashScreen Updation

--- app/src/main/java/com/example/yaddahani/MainActivity.kt
+++ app/src/main/java/com/example/yaddahani/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.yaddahani
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    val viewModel : SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
